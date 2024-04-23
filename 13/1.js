1ee39d2fe379f355a7207052d96e9891a6048cfe
Yozhikvtumane <yoshkin@kot.ks>
2024-04-23 20:30:18 +0200

    dz133 done

--- 13/1.js
+++ 13/1.js
@@ -23,3 +23,32 @@ function createCounter() {
     decrement: () => (count -= 1),
   };
 }
+
+/*
+3) Напишите рекурсивную функцию findElementByClass,
+ которая принимает корневой элемент дерева DOM 
+ и название класса в качестве аргументов и возвращает 
+ первый найденный элемент с указанным классом в этом дереве.
+Пример
+const rootElement = document.getElementById('root');
+const targetElement = findElementByClass(rootElement, 'my-class');
+console.log(targetElement);
+*/
+
+const rootElement = document.getElementById("container");
+
+console.log(rootElement);
+function findElementByClass(rootElement, className) {
+  console.log(rootElement.classList.contains(className));
+  if (rootElement.classList.contains(className)) {
+    return rootElement;
+  }
+  for (let child of rootElement.children) {
+    let found = findElementByClass(child, className);
+    if (found) return found;
+  }
+  return null;
+}
+
+const elem = findElementByClass(rootElement, "find_me_if_you_can");
+console.log(elem);
